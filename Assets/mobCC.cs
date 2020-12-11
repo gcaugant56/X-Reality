606f3e2ff77b0c2ff0aa28fe5c41bcca7d026ee3
Valentin camozzi <camozzi@et.esiea.fr>
2020-12-11 18:13:23 +0100

    -ajout d'un asset pour les mobs (style mannequin) -ajout d'un CharacterControler sur le mob -création d'un script de déplacement rattaché au mob (déplacement uniquement avec flèches haut et bas pour le moment)

---
+++ Assets/mobCC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobCC : MonoBehaviour{
+    public float speed = 1f;
+    //public float gravity = 20f;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start(){
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed; 
+        //moveDirection.y -= gravity * Time.deltaTime;
+        cc.Move(moveDirection * Time.deltaTime);
+    }
+}
