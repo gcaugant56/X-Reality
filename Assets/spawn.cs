f2333fe3c3162d6372b9f23d0e7186ae30a9d826
Guillaume Caugant <caugant@et.esiea.fr>
2020-12-15 14:44:35 +0100

    Spawn des enemies

---
+++ Assets/spawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject enemi;
+    public GameObject create;
+    public float xPos;
+    public float zPos;
+    public int enemyCount;
+    public int nbSpawn;
+    
+    void Start()
+    {
+        nbSpawn = 1;
+        create = spawnEnemi(nbSpawn);
+    }
+
+    public GameObject spawnEnemi(int i)
+    {
+        while(enemyCount < i)
+        {
+            xPos = Random.Range(this.transform.position.x-30, this.transform.position.x +30);
+            zPos = Random.Range(this.transform.position.z-30, this.transform.position.z +30);
+            enemyCount +=1 ;
+            return Instantiate(enemi, new Vector3(xPos,0,zPos), Quaternion.identity);            
+        } 
+        return null;
+    }
+
+    void Update()
+    {
+        if(enemyCount == 0 )
+        {
+            nbSpawn +=1;
+            spawnEnemi(nbSpawn);
+        }
+    }
+    
+}
