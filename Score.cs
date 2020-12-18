441dc152aeb1bcf1b762503701256707eceb11ca
berkantkose <46995466+berkantkose@users.noreply.github.com>
2020-12-18 14:12:46 +0100

    Add files via upload

---
+++ Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    public Text scoreText;
+
+
+    void Awake ()
+    {
+        scoreText = GetComponent<scoreText>();
+        score = 0;
+    }
+ 
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
